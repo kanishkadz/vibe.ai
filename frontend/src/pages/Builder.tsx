aaa41acaa724a8f2336557f5f2698e96bcb567d5
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-19 21:29:33 +0530

    1

--- frontend/src/pages/Builder.tsx
+++ frontend/src/pages/Builder.tsx
@@ -1,12 +1,15 @@
 import TextEditor from '../components/TextEditor';
 import CodePreview from '../components/CodePreview';
 import ChangePrompt from '../components/ChangePrompt';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export const Builder = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='min-h-screen w-full bg-gradient-to-b from-[#b14a84] via-[#000000] to-[#000000] flex flex-col px-6'>
       <header className='pt-8 pb-4'>
-        <img src="Logo.png" alt="Logo" />
+      <img src="/Logo.png" alt="Logo" className="cursor-pointer" onClick={() => navigate('/')} />
       </header>
       <div className='max-w-[1400px] mx-auto px-8 py-6 relative'>
 
