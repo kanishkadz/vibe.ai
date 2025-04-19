51840b883f71117614330650ba1bcec49bce3518
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-19 21:23:54 +0530

    Update Builder.tsx

--- frontend/src/pages/Builder.tsx
+++ frontend/src/pages/Builder.tsx
@@ -5,10 +5,10 @@ import ChangePrompt from '../components/ChangePrompt';
 export const Builder = () => {
   return (
     <div className='min-h-screen w-full bg-gradient-to-b from-[#b14a84] via-[#000000] to-[#000000] flex flex-col px-6'>
+      <header className='pt-8 pb-4'>
+        <img src="Logo.png" alt="Logo" />
+      </header>
       <div className='max-w-[1400px] mx-auto px-8 py-6 relative'>
-        <header className='mb-12'>
-          <img src="Logo.png" alt="Logo" />
-        </header>
 
         <main className='flex gap-8'>
           <TextEditor />
