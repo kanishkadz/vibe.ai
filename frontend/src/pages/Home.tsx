74ff61b451cfa8f6a270b04f46b6a00cc404dd49
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-19 17:13:54 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -11,9 +11,10 @@ export const Home = () => {
       <main className='flex-1 flex flex-col items-center justify-center max-w-5xl mx-auto w-full pb-20'>
         <div className='flex flex-col items-center justify-center'>
           <img src="Icon.png" alt="Icon" />
-          <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold text-[#4e4e4e] text-center mb-8 mt-5'>
+          <h1 className='relative text-4xl md:text-5xl lg:text-6xl font-bold text-transparent stroke-text text-center mb-8 mt-5 hover-reveal'>
             Ready To Bring Your Ideas To Life?
           </h1>
+
           <p className='font-bold text-[#ffffff7c]'>
             Design. Build. and Launch Stunning Websites & Apps - All In One Place
           </p>
