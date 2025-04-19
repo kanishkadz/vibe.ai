87d07ed88467d8f36f7e21f79bc879d725477280
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-19 15:46:00 +0530

    Update Loader.tsx

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,4 +1,3 @@
-
 export function Loader() {
     return <div role="status" className="flex justify-center w-full pt-4">
     <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
