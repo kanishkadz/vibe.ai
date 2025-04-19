bb5d1fcd9de58cdb8913287bb2e3259a5bb64e3d
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-19 21:23:19 +0530

    debug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ import Header from './components/Header';
 function App() {
   return (
     <BrowserRouter>
-    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/builder" element={<Builder />} />
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 const Header = () => {
-  return (
-    <div className='bg-[#b14a84] pt-5 pb-4 pl-5 flex'>
-
+    return (
+      <div className='fixed top-0 left-0 w-full bg-[#b14a84] pt-5 pb-4 pl-5 flex shadow-md z-50'>
         <img src="/Logo.png" alt="Logo" />
-    </div>
-  )
-}
-
-export default Header
+      </div>
+    );
+  };
+  
+  export default Header;
+  
