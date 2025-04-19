372ba1f825576ef429484f3f6e20865f643697e8
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-19 16:56:48 +0530

    Update TextEditor.tsx

---
+++ frontend/src/components/TextEditor.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TextEditor = () => {
+  return (
+    <div>
+        <div className='w-full max-w-md bg-[#1a1a1a] rounded-xl p-6 text-white'>
+            <p className='leading-relaxed'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab placeat voluptates id, deleniti explicabo sint quod dignissimos iusto aperiam autem recusandae error tempora expedita repellendus facilis ea ipsum! Harum hic repellendus ratione odit iste quae cum aut at mollitia sequi aliquid consectetur praesentium doloremque eveniet dolorem sunt recusandae nulla iusto accusantium, ipsum quia dicta maiores quibusdam culpa. Quia eaque et magnam quam sit porro, odio corrupti quis maiores eos repellat officiis facere? Eaque nihil mollitia enim? Perspiciatis cupiditate nesciunt laudantium, repellat explicabo quasi odit pariatur odio dignissimos autem mollitia distinctio eos. Illo illum ipsam aliquam amet incidunt eius, sequi quos blanditiis laborum voluptate aliquid numquam autem, assumenda architecto, error nesciunt. Soluta in veniam earum ipsa nesciunt quidem exercitationem fugiat sapiente magnam, suscipit ab ad natus. Eum accusantium sapiente, amet voluptatum ea similique ut vitae autem tenetur odit totam. Odio iure, accusantium quibusdam voluptas dolorem magnam eos? Enim sit dolorem porro sapiente beatae voluptatum similique ex ducimus. Quas sunt quisquam aliquam dicta voluptate esse deleniti harum! Laboriosam, impedit pariatur, deleniti minus omnis laudantium earum reiciendis voluptate perspiciatis, sed perferendis excepturi natus facere voluptates provident ipsam suscipit fugit? Nemo quidem consectetur blanditiis voluptas debitis beatae accusantium! Doloribus consectetur aspernatur mollitia sequi ut.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default TextEditor
