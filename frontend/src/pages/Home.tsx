b5229aee7ee62ea3da16f95705d5fba9de2c3124
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-19 16:43:13 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Sparkles } from 'lucide-react';
 
 export const Home = () => {
   return (
-    <div className='min-h-screen w-full bg-gradient-to-b from-[#e6b3d1] via-[#d9c1d9] to-[#d1d1d1] flex flex-col px-6'>
+    <div className='min-h-screen w-full bg-gradient-to-b from-[#b14a84] via-[#000000] to-[#000000] flex flex-col px-6'>
       <header className='pt-8 pb-4'>
         <img src="Logo.png" alt="Logo" />
       </header>
@@ -11,7 +11,7 @@ export const Home = () => {
       <main className='flex-1 flex flex-col items-center justify-center max-w-5xl mx-auto w-full pb-20'>
         <div className='flex flex-col items-center justify-center'>
           <img src="Icon.png" alt="Icon" />
-          <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold text-[#333333] text-center mb-8'>
+          <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold text-[#3c3c3c] text-center mb-8'>
             Ready To Bring Your Ideas To Life?
           </h1>
           <p className='font-bold text-white'>
@@ -25,7 +25,7 @@ export const Home = () => {
             <input
               type="text"
               placeholder='What do you want to build today?'
-              className='w-full py-5 px-12 bg-[#1a1a1a] text-white rounded-xl focus:outline-none focus:ring-2 focus:ring-[#ff3e9a] transition-all duration-300 placeholder-gray-400 text-lg'
+              className='w-full py-5 px-12 bg-[#1a1a1a] text-white rounded-3xl focus:outline-none focus:ring-2 focus:ring-[#ff3e9a] transition-all duration-300 placeholder-gray-400 text-lg'
             />
           </div>
         </div>
